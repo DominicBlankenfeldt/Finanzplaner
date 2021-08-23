b5f62921311424b23af5cffa642a1a75457e3613
Dominic.Blankenfeldt <d.blankenfeldt@mbd-team.de>
2021-08-23 15:19:28 +0200

    problem mit dem image aus neuen plänen gefixt mit hilfe einer quelle svg mit quelltext im svg datei

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ function newRowMinus() {
 function newPlan() {
 
     let newPlanElement = document.createElement("div");
-    let newImgForChild = document.createElement("svg");
+    let newImgForChild = document.createElement("img");
     let newPlanElementName = document.createElement("div");
     let newPlanName = document.getElementById("textName").value
     let newPlanNameBig = document.createElement("h5")
@@ -109,24 +109,26 @@ function newPlan() {
     newPlanElement.classList.add("carousel-item")
     newPlanElement.appendChild(newImgForChild)
 
-    newImgForChild.setAttribute("xmlns", "http://www.w3.org/2000/svg")
-    newImgForChild.setAttribute("width", "275")
-    newImgForChild.setAttribute("height", "275")
-    newImgForChild.setAttribute("fill", "currentColor")
-    newImgForChild.classList.add("bi", "bi-cash-coin")
-    newImgForChild.setAttribute("viewBox", "0 0 16 16")
-    // newImgForChild.src = "https://accentsconagua.com/img/images/how-to-build-a-full-screen-responsive-carousel-slider-with-owljs_4.png"
-    // newImgForChild.classList.add("m-auto")
-    // newImgForChild.classList.add("d-block")
-    newImgForChild.appendChild(newPlanPath1)
-    newPlanPath1.setAttribute("fill-rule", "evenodd")
-    newPlanPath1.setAttribute("d", "M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z")
-    newImgForChild.appendChild(newPlanPath2)
-    newPlanPath2.setAttribute("d", "M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z")
-    newImgForChild.appendChild(newPlanPath3)
-    newPlanPath3.setAttribute("d", "M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z")
-    newImgForChild.appendChild(newPlanPath4)
-    newPlanPath4.setAttribute("d", "M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z")
+    // newImgForChild.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+    // newImgForChild.setAttribute("width", "275")
+    // newImgForChild.setAttribute("height", "275")
+    // newImgForChild.setAttribute("fill", "currentColor")
+    // newImgForChild.classList.add("bi", "bi-cash-coin")
+    // newImgForChild.setAttribute("viewBox", "0 0 16 16")
+    newImgForChild.src = "cash.svg"
+    newImgForChild.classList.add("m-auto")
+    newImgForChild.classList.add("d-block")
+    // newImgForChild.appendChild(newPlanPath1)
+    // newPlanPath1.setAttribute("fill-rule", "evenodd")
+    // newPlanPath1.setAttribute("d", "M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z")
+    // newImgForChild.appendChild(newPlanPath2)
+    // newPlanPath2.setAttribute("d", "M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z")
+    // newImgForChild.appendChild(newPlanPath3)
+    // newPlanPath3.setAttribute("d", "M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z")
+    // newImgForChild.appendChild(newPlanPath4)
+    // newPlanPath4.setAttribute("d", "M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z")
+
+    // path wird nicht richtig übernommen, wird mit closingtag generiert und ohne / an der schlussstelle
 
     newPlanNameBig.innerHTML = newPlanName
     newPlanElementName.appendChild(newPlanNameBig)
@@ -141,3 +143,9 @@ function newPlan() {
     console.log("neuer Plan erfolgreich erstellt")
 }
 
+
+// löschenfunktion für ausgewählten plan
+
+function planLöschen() {
+
+}
