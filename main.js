7bd1ddb118c22f9e7980f2aa670fdaa371df971f
Dominic.Blankenfeldt <d.blankenfeldt@mbd-team.de>
2021-08-26 10:23:33 +0200

    bei neuem Plan werden nun alle werte übernommen und in der info angezeigt

--- main.js
+++ main.js
@@ -100,17 +100,56 @@ function newPlan() {
     let newImgForChild = document.createElement("img");
     let newPlanElementName = document.createElement("div");
     let newPlanName = document.getElementById("textName").value
-    let newPlanNameBig = document.createElement("h5")
+
+    let newPlanErstellenDate = document.getElementById("erstellenDate").value
+    let newPlanGeld = document.getElementById("geld").value
+    let newPlanNotiz = document.getElementById("notiz").value
+
+    let newPlanDivDate = document.createElement("div")
+    let newPlanDivGeld = document.createElement("div")
+    let newPlanDivNotiz = document.createElement("div")
+    let newPlanDivName = document.createElement("div")
+
+    let newPlanNameBig0 = document.createElement("h5")
+    let newPlanNameBig1 = document.createElement("h5")
+    let newPlanNameBig2 = document.createElement("h5")
+    let newPlanNameBig3 = document.createElement("h5")
+
 
     newPlanElement.classList.add("carousel-item")
     newPlanElement.appendChild(newImgForChild)
     newImgForChild.src = "cash.svg"
     newImgForChild.classList.add("m-auto")
     newImgForChild.classList.add("d-block")
-    newPlanNameBig.innerHTML = newPlanName
-    newPlanElementName.appendChild(newPlanNameBig)
-    newPlanElementName.classList.add("m-auto")
-    newPlanElementName.classList.add("d-block")
+
+    newPlanNameBig0.innerHTML = newPlanName
+    newPlanElementName.appendChild(newPlanDivName)
+    newPlanDivName.classList.add("col-4")
+    newPlanDivName.innerHTML = "Name:"
+    newPlanDivName.appendChild(newPlanNameBig0)
+
+    newPlanNameBig1.innerHTML = newPlanErstellenDate
+    newPlanElementName.appendChild(newPlanDivDate)
+    newPlanDivDate.classList.add("col-4")
+    newPlanDivDate.innerHTML = "Erstellt am:"
+    newPlanDivDate.appendChild(newPlanNameBig1)
+
+    newPlanNameBig2.innerHTML = newPlanGeld + "€"
+    newPlanElementName.appendChild(newPlanDivGeld)
+    newPlanDivGeld.classList.add("col-4")
+    newPlanDivGeld.innerHTML = "Sparziel in €:"
+    newPlanDivGeld.appendChild(newPlanNameBig2)
+
+    newPlanNameBig3.innerHTML = newPlanNotiz
+    newPlanElementName.appendChild(newPlanDivNotiz)
+    newPlanDivNotiz.classList.add("col-12")
+    newPlanDivNotiz.innerHTML = "Notiz:"
+    newPlanDivNotiz.appendChild(newPlanNameBig3)
+
+    // newPlanElementName.classList.add("m-auto")
+    // newPlanElementName.classList.add("d-block")
+    newPlanElementName.classList.add("row")
+
     newPlanElement.setAttribute("id", newPlanName)
     newPlanElement.appendChild(newPlanElementName)
     document.getElementById("carousel-inner").appendChild(newPlanElement)
@@ -131,9 +170,7 @@ function newPlan() {
 let findId = null
 
 function planDelete() {
-    document.getElementById("examplePlan").classList.add(active)
+    // document.getElementById("examplePlan").classList.add(active)
     node = document.getElementById(findId)
     node.remove()
-
-
 }
